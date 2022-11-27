13e830791190dcd5e96389d764144af36a7d89b8
Sergio Rios <serggio.rs@gmail.com>
2022-11-27 20:50:51 -0300

    update challenge_repository functions

---
+++ 5 functions.js
@@ -0,0 +1,38 @@
+//Function declaration
+function func() {
+    return 'hello world';
+ };
+ const result = func();
+ console.log(result);
+ //Function expression
+ const func = function() {
+    return 'hello world';
+ };
+ const result = func();
+ console.log(result);
+ //Correctly return value from function (1)
+ const func = function() {
+    let text = 'hello';
+    text = text + ' world';
+    return text;
+ };
+ const result = func();
+ console.log(result);
+ //Correctly return value from function (2)
+ const func = function () {
+    let text = 'hello';
+    text = text + ' world';
+    return text;
+ };
+ const result = func();
+ console.log(result);
+ //Functions - parameters and arguments (1)
+ const multiply = function (num) {
+    return num * num;
+ };
+  
+ const arg = 5;
+  
+ const result = multiply(arg);
+ console.log(result === 25);
+ 
